c4f30059e95eed14c7950b002d70becc9d2e60e0
Eduardo Tarancón Delgado <eduardo.tarancon.contractor@bbva.com>
2018-01-25 10:37:33 +0100

    show detail when an item list is clicked

--- app.js
+++ app.js
@@ -9,61 +9,82 @@ function initApplication() {
     const URL = 'cats.json';
     let catsList = [];
     const listContainer = document.getElementById('list-container');
+    const detailContainer = document.getElementById('detail-container');
 
     //Get list of cats to show in index
-    _generateList(URL).then((response) => {
+    _getData(URL).then((response) => {
         catsList = response.cats;
 
         catsList.forEach(data => {
             //Crear los elementos en el DOM
-            let node = _createNode(data);
+            let node = _createHtml(data, 'li');
             //Añadir event
             node.addEventListener('click', function (target) {
-                _addClick(target.currentTarget, catsList);
+                selectItem(target.currentTarget, catsList);
             });
             //Pintar
             listContainer.appendChild(node);
         });
     });
+
+    function selectItem(target, catsList) {
+        const id = parseInt(target.getAttribute('data-id'));
+        _findById(id, catsList).then(cat => {
+            if (cat) {
+                cat.catCount++;
+    
+                const span = target.getElementsByTagName('span')[0];
+                span.innerText = cat.catCount;
+    
+                showDetail(detailContainer, cat);
+            }
+        });
+    
+    }
+    
+    function showDetail(container, detail = {}) {
+        if (!container) {
+            container = document.getElementsByTagName('body')[0];
+        }
+        const element = _createHtml(detail, 'div');
+        
+        const child = container.getElementsByTagName('div')[0];
+        if(child){
+            container.removeChild(child);
+        }
+        container.appendChild(element);
+    }
 }
 
 //Private  Functions
-function _getData(url) {
+function _get(url) {
     return fetch(url, {
         method: 'get'
     });
 }
 
-function _generateList(url) {
-    return _getData(url).then((response) => {
+function _getData(url) {
+    return _get(url).then((response) => {
         return response.json();
     });
 }
 
-function _createNode(data) {
-    let node = document.createElement('li');
-    node.setAttribute('data-id', data.id);
-    node.className = 'list-item';
-    node.innerHTML = `${data.catName} <img src="${data.catImage}"/><span>${data.catCount}</span>`;
-
-    return node;
-}
-
 function _findById(id, list) {
-    return list.find(function (obj) {
-        if (obj.id && obj.id === id) {
-            return true;
-        }
+    return new Promise(function (resolve, reject) {
+        const item = list.find(function (obj) {
+            if (obj.id && obj.id === id) {
+                return true;
+            }
+        });
+        resolve(item);
     });
 }
 
-function _addClick(target, catsList) {
-    const id = parseInt(target.getAttribute('data-id'));
-    const cat = _findById(id, catsList);
-    if (cat) {
-        cat.catCount++;
-    }
-    const span = target.getElementsByTagName('span')[0];
-    span.innerText = cat.catCount;
+function _createHtml(data, tagElement = 'div') {
+    let node = document.createElement(tagElement);
+    node.setAttribute('data-id', data.id);
+    node.className = 'list-item';
+    node.innerHTML = `<h2>${data.catName}</h2> <img src="${data.catImage}"/><span>${data.catCount}</span>`;
 
+    return node;
 }
